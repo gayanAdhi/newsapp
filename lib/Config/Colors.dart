86608d801123c2657ae2b0fb15979554ea8aac05
AMGC ADHIKARI <140136026+gayanAdhi@users.noreply.github.com>
2024-07-10 10:49:25 +0530

    ..

--- lib/Config/Colors.dart
+++ lib/Config/Colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-//😍 Light Theme Color
+// Light Theme Color
 const lightBgColor = Color(0xffFFFFFF);
 const lightFontColor = Color(0xff191B28);
 const lightDivColor = Color(0xffF5F6FA);
 const lightPrimaryColor = Color(0xff0057FF);
 const lightLableColor = Color(0xff8E8E8E);
 
-//😍  Dark Theme Color
+// Dark Theme Color
 const darkBgColor = Color(0xff191B28);
 const darkFontColor = Color(0xffFFFFFF);
 const darkDivColor = Color(0xff34374B);
